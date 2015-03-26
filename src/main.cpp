6686ee11863f9ea91b07c28b0435732190968567
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2015-03-26 05:38:39 -0400

    Block Time, MaxMoney(Total Coin) etc. parameter changed.

--- src/main.cpp
+++ src/main.cpp
@@ -1087,9 +1087,9 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 50 * COIN;
+    int64 nSubsidy = 100 * COIN;
 
-    // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
+    // Subsidy is cut in 3/4 every 840000 blocks, which will occur approximately every 1 years
     nSubsidy >>= (nHeight / 840000); // SherlockHolmesCoin: 840k blocks in ~4 years
 
     return nSubsidy + nFees;
